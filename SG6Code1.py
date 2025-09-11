3a2f3a527fe0509f025c623d8666a4688d8bad6c
FrancisJAlboroto <francisarvie.alboroto@smc.pshs.edu.ph>
2025-09-11 13:22:41 +0800

    Add files via upload
    This code asks for 3 numbers, stores it in 3 variables, and calculats for sum.

---
+++ SG6Code1.py
@@ -0,0 +1,9 @@
+num1 = 0.0
+num2 = 0.0
+num3 = 0.0
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+sum = num1 + num2 + num3
+print("The total of the three numbers is:", sum)
